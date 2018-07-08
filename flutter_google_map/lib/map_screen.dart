ac4b11fd0ee93afb30798f807a8d5ad563bbb443
Sunil <sunilkmrnishad@gmail.com>
2018-07-08 11:28:01 -0700

    Change UI.
    Wokred on google map UI.

--- flutter_google_map/lib/map_screen.dart
+++ flutter_google_map/lib/map_screen.dart
@@ -3,18 +3,17 @@ import 'package:flutter_google_map/map_util.dart';
 import 'package:map_view/map_view.dart';
 
 class MapScreen extends StatefulWidget {
-
   @override
   _MapScreenState createState() => new _MapScreenState();
 }
 
 class _MapScreenState extends State<MapScreen> {
 
+  final teLatitude = TextEditingController();
+  final teLongitude = TextEditingController();
+  final teZoomLevel = TextEditingController();
   MapView mapView;
   MapUtil mapUtil;
-
-
-
 
   @override
   void initState() {
@@ -23,73 +22,131 @@ class _MapScreenState extends State<MapScreen> {
     mapUtil = new MapUtil();
     mapUtil.init();
     mapView = new MapView();
-    mapView.onMapTapped.listen((_) {
-      setState(() {
-        mapView.setMarkers(mapUtil.getMarker());
-        mapView.zoomToFit(padding: 100);
-      });
-    });
-
-    mapView.onCameraChanged.listen((cameraPosition) =>
-        this.setState(() => mapUtil.cameraPosition = cameraPosition));
+    teLatitude.text = mapUtil.getCamera().center.latitude.toString();
+    teLongitude.text = mapUtil.getCamera().center.longitude.toString();
+    teZoomLevel.text = mapUtil.getCamera().zoom.toString();
 
   }
 
   @override
   Widget build(BuildContext context) {
+    var screenWidget = new Column(
+      children: <Widget>[
+        new Container(
+          height: 230.0,
+          child: new Stack(
+            children: <Widget>[
+              new Center(
+                child: Container(
+                  child: new Text(
+                    "Google Map Box",
+                    textAlign: TextAlign.center,
+                  ),
+                  padding: const EdgeInsets.all(20.0),
+                ),
+              ),
+              new GestureDetector(
+                onTap: () => mapUtil.showMap(mapView),
+                child: new Center(
+                  child: new Image.network(mapUtil.getStaticMap().toString()),
+                ),
+              ),
+            ],
+          ),
+        ),
+        new Container(
+          margin: new EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 0.0),
+          padding: new EdgeInsets.only(top: 10.0),
+          child: new Text(
+            "Change Lat and long for get your location map",
+            style: new TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ),
+        new Container(
+          padding: new EdgeInsets.all(20.0),
+          child: new Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              getTextField("latitude", teLatitude),
+              getTextField("Lontitude", teLongitude),
+              getTextField("Zoom level", teZoomLevel),
+              new GestureDetector(
+                onTap: () => updateStaticMap(),
+                child: new Container(
+                  margin: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+                  child: getButton("Get Static map",
+                      EdgeInsets.fromLTRB(0.0, 30.0, 0.0, 0.0)),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+
     return new Scaffold(
       appBar: AppBar(
-        title: Text("Flutter Google map",
-        style: new TextStyle(
-          color: const Color(0xFFFFFFFF),
-        ),
+        title: new Text(
+          "Flutter Google maps",
+          textAlign: TextAlign.center,
+          style: new TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
         ),
       ),
-      body: new Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-
-        children: <Widget>[
-
-          new Container(
-            height: 300.0,
-
-            child: new Stack(
-              children: <Widget>[
-
-                new Center(
-                  child: Container(
-
-                    child: new Text(
-                      "Map should show here",
-                      textAlign: TextAlign.center,
-                    ),
-                    padding: const EdgeInsets.all(20.0),
-                  ),
-                ),
-                new InkWell(
-                  child: new Center(
-                    child: new Image.network(mapUtil.getStaticMap().toString()),
-                  ),
-                  onTap: mapUtil.showMap(mapView),
-                )
-              ],
-            ),
-          ),
-          new Container(
-            padding: new EdgeInsets.only(top: 10.0),
-            child: new Text(
-              "Tap the map to interact",
-              style: new TextStyle(fontWeight: FontWeight.bold),
-            ),
-          ),
-          new Container(
-            padding: new EdgeInsets.only(top: 25.0),
-            child: new Text("Camera Position: \n\nLat: ${mapUtil.getCamera().center
-                    .latitude}\n\nLng:${mapUtil.getCamera().center
-                    .longitude}\n\nZoom: ${mapUtil.getCamera().zoom}"),
-          ),
-        ],
+      body: new SingleChildScrollView(
+        child: screenWidget,
       ),
     );
+  }
+
+  Widget getTextField(
+      String inputBoxName, TextEditingController inputBoxController) {
+    var loginBtn = new Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: new TextFormField(
+        controller: inputBoxController,
+        decoration: new InputDecoration(
+          hintText: inputBoxName,
+        ),
+      ),
+    );
+
+    return loginBtn;
+  }
+
+  Widget getButton(String buttonLabel, EdgeInsets margin) {
+    var staticMapBtn = new Container(
+      margin: margin,
+      padding: EdgeInsets.all(8.0),
+      alignment: FractionalOffset.center,
+      decoration: new BoxDecoration(
+        color: const Color(0xFF167F67),
+        border: Border.all(color: const Color(0xFF28324E)),
+        borderRadius: new BorderRadius.all(const Radius.circular(6.0)),
+      ),
+      child: new Text(
+        buttonLabel,
+        style: new TextStyle(
+          color: const Color(0xFFFFFFFF),
+          fontSize: 20.0,
+          fontWeight: FontWeight.w300,
+          letterSpacing: 0.3,
+        ),
+      ),
+    );
+    return staticMapBtn;
+  }
+
+  updateStaticMap() {
+
+    mapUtil.updateLocation(new Location(double.tryParse(teLatitude.text) ?? 0.0, double.tryParse(teLongitude.text) ?? 0.0));
+    mapUtil.updateZoomLevel(double.tryParse(teZoomLevel.text) ?? 12.0);
+    setState(() {
+
+    });
+
   }
 }
--- flutter_google_map/lib/map_util.dart
+++ flutter_google_map/lib/map_util.dart
@@ -2,77 +2,75 @@ import 'package:flutter/material.dart';
 import 'package:map_view/map_view.dart';
 
 class MapUtil {
-
-
-  static var api_key = "AIzaSyDrHKl8IxB4cGXIoELXQOzzZwiH1xtsRf4";
+  static var api_key = "Google_map_key";
   var staticMapProvider;
   CameraPosition cameraPosition;
+  var location = new Location(30.7269882, 76.8354053);
+  var zoomLevel = 12.0;
 
-  init(){
+  init() {
     staticMapProvider = new StaticMapProvider(MapUtil.api_key);
-
-    cameraPosition =
-    new CameraPosition(getMyLocation(), 12.0);
+    cameraPosition = new CameraPosition(getMyLocation(), zoomLevel);
   }
 
   List<Marker> getMarker() {
-
     List<Marker> markers = <Marker>[
-      new Marker("1", "The Lalit", 30.7269882,76.8354053,
-          color: Colors.amber),
-      new Marker("2", "Tech mahindra", 30.7269882,76.8354053,
-          color: Colors.lightGreen),
-      new Marker("3", "Infosys", 30.7269882,76.8354053,
-          color: Colors.green),
+      new Marker("1", "The Lalit", 30.7265995, 76.8361955, color: Colors.amber),
+      new Marker("2", "Tech mahindra", 30.7290226, 76.8339204,
+          color: Colors.red),
+      new Marker("3", "Infosys", 30.7285108, 76.8388771, color: Colors.green),
     ];
 
     return markers;
-
   }
 
-  Uri getStaticMap(){
-    return staticMapProvider.getStaticUri(
-        getMyLocation(), 12,
+  Uri getStaticMap() {
+    return staticMapProvider.getStaticUri(getMyLocation(), zoomLevel.toInt(),
         height: 400, width: 900);
   }
 
-  Location getMyLocation(){
-    return new Location(30.7269882, 76.8354053);
+  Location getMyLocation() {
+    return location;
   }
 
-  CameraPosition  getCamera() {
+  CameraPosition getCamera() {
     return cameraPosition;
   }
 
-
   showMap(MapView mapView) {
+    mapView.show(
+        new MapOptions(
+            mapViewType: MapViewType.normal,
+            initialCameraPosition: getCamera(),
+            showUserLocation: true,
+            title: "Recent Location"),
+        toolbarActions: [new ToolbarAction("Close", 1
 
-    mapView.show(new MapOptions(
-        mapViewType: MapViewType.normal,
 
-        initialCameraPosition:
-        getCamera(),
-        showUserLocation: true,
-        title: "Recent Location"),
-        toolbarActions: [new ToolbarAction("Close", 1)]
+        )]
+
     );
 
-    mapView.setMarkers(getMarker());
+
     mapView.zoomToFit(padding: 100);
 
     mapView.onMapReady.listen((_) {
+      mapView.setMarkers(getMarker());
       print("Map ready");
     });
 
-    mapView.onLocationUpdated.listen((location) => print("Location updated $location"));
+    mapView.onLocationUpdated.listen((location) => updateLocation(location));
 
     mapView.onTouchAnnotation.listen((marker) => print("marker tapped"));
 
-    mapView.onMapTapped
-        .listen((location) => print("Touched location $location"));
-
-
-
+    mapView.onMapTapped.listen((location) => updateLocation(location));
   }
 
+  updateLocation(Location location) {
+    this.location = location;
+    print("location changed $location");
+  }
+  updateZoomLevel(double zoomLevel) {
+    this.zoomLevel = zoomLevel;
+  }
 }
